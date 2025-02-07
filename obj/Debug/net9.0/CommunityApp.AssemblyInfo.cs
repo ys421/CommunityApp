e0375d67b5b69652662a8b917f7136bd7ce2cd94
ys421 <ychung47@my.bcit.ca>
2025-02-07 15:28:32 -0800

    first commit

--- obj/Debug/net9.0/CommunityApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/CommunityApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommunityApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99b43a2737e90a9c0b088a1585b4dfe0b74cb2f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommunityApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommunityApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
